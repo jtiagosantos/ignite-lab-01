4ce053b855ecfdf8b9fcb628214a01e2c9e0ed70
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2022-04-11 20:25:07 -0400

    class 04

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from 'next/app';
 import { UserProvider } from '@auth0/nextjs-auth0';
 
+import '../styles/global.css';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
--- frontend/src/pages/app/index.tsx
+++ frontend/src/pages/app/index.tsx
@@ -1,15 +1,30 @@
 import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { useMeQuery } from '../../graphql/generated/graphql';
+import { ssrGetProducts } from '../../graphql/generated/page';
+import { withApollo } from '../../services/withApollo';
 
-export default function Home() {
+function Home({ data }) {
   const { user } = useUser();
+  const { data: me } = useMeQuery();
 
   return (
     <div>
       <h1>Hello World</h1>
-
+      user: <pre>{JSON.stringify(me, null, 2)}</pre>
+      {/* <pre>{JSON.stringify(data.products, null, 2)}</pre> */}
       <pre>{JSON.stringify(user, null, 2)}</pre>
     </div>
   );
 }
 
-export const getServerSideProps = withPageAuthRequired();
+export const getServerSideProps = withPageAuthRequired({
+  getServerSideProps: async (ctx) => {
+    // getServerPageGetProducts({}, ctx)
+
+    return {
+      props: {},
+    };
+  },
+});
+
+export default withApollo(ssrGetProducts.withPage()(Home));
